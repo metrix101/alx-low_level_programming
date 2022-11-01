3b84f548fea4f54f6a5417bfa36d4af9d13d4604
Morgan <metrix2018@gmail.com>
2022-11-01 02:46:43 +0100

    fixing elf task

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -257,6 +257,9 @@ void close_elf(int elf)
  * main - Displays the information contained in the
    ELF header at the start of an ELF file.
  * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: 0 on success.
+ * Description: If the file is not an ELF File or
    the function fails - exit code 98.
  */
 
